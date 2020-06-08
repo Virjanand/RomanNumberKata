0979b1f1aac2534ef77cf851a4186856678c81fb
Virjanand <virjanand@virjanand.com>
2020-06-08 05:10:00 +0200

    Red test: 1 -> I

---
+++ src/main/java/RomanNumber/RomanNumber.java
@@ -0,0 +1,7 @@
+package RomanNumber;
+
+public class RomanNumber {
+    public String toRoman(int i) {
+        return null;
+    }
+}
---
+++ src/test/java/RomanNumber/RomanNumberTest.java
@@ -0,0 +1,14 @@
+package RomanNumber;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumberTest {
+
+    @Test
+    void oneGivesI() {
+        RomanNumber romanNumber = new RomanNumber();
+        assertEquals("I", romanNumber.toRoman(1));
+    }
+}
