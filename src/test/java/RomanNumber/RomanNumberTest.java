5c5f797ce2c44c3149f92a41ade56515e2fca556
Virjanand <virjanand@virjanand.com>
2020-06-10 05:09:22 +0200

    Green test: 5 -> V

--- src/test/java/RomanNumber/RomanNumberTest.java
+++ src/test/java/RomanNumber/RomanNumberTest.java
@@ -1,6 +1,5 @@
 package RomanNumber;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,7 +10,8 @@ public class RomanNumberTest {
     @ParameterizedTest(name = "{0} -> {1}")
     @CsvSource({
             "1, I",
-            "2, II"
+            "2, II",
+            "5, V"
     })
     void oneGivesI(int arabicNumber, String expectedRomanNumber) {
         RomanNumber romanNumber = new RomanNumber();
